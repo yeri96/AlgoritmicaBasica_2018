6624ecafc96f1109615a74fd9001553a9a5378ba
Gerald Franco Barba <grldfrnc@gmail.com>
2018-03-06 13:11:54 +0100

    Create swap.py

---
+++ swap/swap.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+mano_der = "movil"
+mano_izq = "bocadillo"
+mano_extra = mano_der
+mano_der = mano_izq
+mano_izq = mano_extra
+
+print "En la mano izquierda tengo el ", mano_izq
+print "En la mano derecha tengo el ", mano_der
