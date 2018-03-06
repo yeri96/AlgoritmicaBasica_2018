1a6e7889b5b23678a85a2f47f1776efcb922eae6
Gerald Franco Barba <grldfrnc@gmail.com>
2018-03-06 12:38:33 +0100

    Create condición_chunga.py

---
+++ condici-n_chunga/condición_chunga.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+numero = input("Introduce un número: ")
+
+
+if numero%2 == 0 and numero > -10 and numero < 40 :
+    print "Este número es correcto"
+else:
+    print "Incorrecto"
+    print "Intentalo de nuevo"
