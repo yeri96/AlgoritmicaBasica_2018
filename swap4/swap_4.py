d2cca770736f521316d22d4eb27c9044874dabd9
Gerald Franco Barba <grldfrnc@gmail.com>
2018-03-06 13:30:51 +0100

    Create swap_4.py

---
+++ swap4/swap_4.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+cajon1 = "movil"
+cajon2 = "bocadillo"
+cajon3 = "boli"
+cajon4 = "bebida"
+cajon_extra1 = cajon1
+cajon1 = cajon3
+cajon3 = cajon_extra1
+cajon_extra2 = cajon2
+cajon2 = cajon4
+cajon4 = cajon_extra2
+
+print cajon1
+print cajon2
+print cajon3
+print cajon4
