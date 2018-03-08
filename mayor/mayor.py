0e125237fba2c83353a7de8319d369bd9cf82fb9
Gerald Franco Barba <grldfrnc@gmail.com>
2018-03-08 16:28:19 +0100

    Create mayor.py

---
+++ mayor/mayor.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+valor1 = input("Introduce el primer valor: ")
+valor2 = input("Introduce el segundo valor: ")
+
+if valor1 > valor2:
+    print "el número más alto es :", valor1
+else:
+    if valor1 == valor2:
+        print "Ambos número son iguales"
+    else:
+	print "El número más alto es :", valor2
